81204fa2f73dc8cc7c68b3050af687f16cf163f3
iandjones81 <iandjones81@gmail.com>
2025-02-19 22:08:15 +0000

    PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+          name: 'Jones Ticks',
+          short_name: 'Jones Ticks',
+          description: 'An application for the Jones family to track chores',
+          theme_color: '#ffffff',
+          icons: [
+            {
+                src: 'checkmark-small.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+              src: 'checkmark.png',
+              sizes: '512x512',
+              type: 'image/png',
+            }
+          ],
+      },
+  })
+  ],
 })
+
